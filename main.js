c321e2f0136fd43ea947dcedef31bb796e16575a
shundroid <ra24483@qb3.so-net.ne.jp>
2015-12-21 19:00:50 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+var ip = "127.0.0.1";
+var port = 10000;
+
+var contentTypes = {
+  "html": "text/html",
+  "css": "text/css",
+  "js": "text/javascript",
+  "json": "application/json",
+  "png": "image/png",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpeg",
+  "gif": "image/gif"
+}
+var http = require("http");
+var fs = require("fs");
+console.log("server running...");
+http.createServer(function(req, res) {
+  var url = req.url === "/" ? "/index.html" : req.url;
+  res.writeHead(200, { "Content-Type": contentTypes[url.split(".")[1]]});
+  fs.readFile(__dirname + "/public" + url, "utf-8", function(err, data) {
+    res.write(data);
+    res.end();
+  });
+}).listen(port, ip);
